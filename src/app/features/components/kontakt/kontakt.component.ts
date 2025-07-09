208e033659290b9ce3173a67ddc5fc09b671fee7
mrkimedo69 <luka.mavric26051999@gmail.com>
2025-07-09 20:08:26 +0200

    Added state management to homepage, contact page and 3d designer page

--- src/app/features/components/kontakt/kontakt.component.ts
+++ src/app/features/components/kontakt/kontakt.component.ts
@@ -1,38 +1,38 @@
 import { Component } from '@angular/core';
-import emailjs from 'emailjs-com';
-
+import { KontaktStore } from './store/kontak.store';
 @Component({
   selector: 'app-kontakt',
   templateUrl: './kontakt.component.html',
   styleUrls: ['./kontakt.component.css']
 })
 export class KontaktComponent {
-  ime: string = '';
-  email: string = '';
-  poruka: string = '';
+  constructor(public store: KontaktStore) {}
+
+  get ime() {
+    return this.store.ime();
+  }
+
+  set ime(value: string) {
+    this.store.ime.set(value);
+  }
+
+  get email() {
+    return this.store.email();
+  }
+
+  set email(value: string) {
+    this.store.email.set(value);
+  }
+
+  get poruka() {
+    return this.store.poruka();
+  }
+
+  set poruka(value: string) {
+    this.store.poruka.set(value);
+  }
 
   posaljiPoruku() {
-    if (!this.ime || !this.email || !this.poruka) {
-      alert('Molimo ispunite sva polja.');
-      return;
-    }
-
-    const templateParams = {
-      from_name: this.ime,
-      from_email: this.email,
-      message: this.poruka
-    };
-
-    emailjs.send('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', templateParams, 'YOUR_PUBLIC_KEY')
-      .then((response) => {
-        console.log('Email poslan!', response.status, response.text);
-        alert('Hvala na poruci! Javit ćemo vam se uskoro.');
-        this.ime = '';
-        this.email = '';
-        this.poruka = '';
-      }, (err) => {
-        console.error('Greška pri slanju', err);
-        alert('Došlo je do greške. Pokušajte kasnije.');
-      });
+    this.store.sendMessage();
   }
 }
